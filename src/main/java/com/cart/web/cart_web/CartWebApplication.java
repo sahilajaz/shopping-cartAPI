489f9a10765461555c3335fe1e229fd48b1d0937
sahilajaz <sahilajazw@gmail.com>
2024-10-31 16:15:39 +0530

    first commit

---
+++ src/main/java/com/cart/web/cart_web/CartWebApplication.java
@@ -0,0 +1,13 @@
+package com.cart.web.cart_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartWebApplication.class, args);
+	}
+
+}
